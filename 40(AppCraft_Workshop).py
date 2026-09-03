d9b374d5573828835c49751161bbd059162495b0
harsheeyyyssss84 <harshitdadhich2008@gmail.com>
2026-09-03 14:21:59 +0530

    eleventh commit

---
+++ 40(AppCraft_Workshop).py
@@ -0,0 +1,16 @@
+def fibo(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    
+    fib_sequence = [0, 1]
+    for i in range(2, n):
+        next_fib = fib_sequence[i - 1] + fib_sequence[i - 2]
+        fib_sequence.append(next_fib)
+    
+    return fib_sequence
+
+print(fibo(int(input("Enter the number of Fibonacci terms to generate: "))))
